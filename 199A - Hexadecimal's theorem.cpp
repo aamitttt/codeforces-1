ba3345b3f4562ea647d9c7cc0367791a14048601
Amit kumar <88128579+aamitttt@users.noreply.github.com>
2021-09-09 23:27:20 +0530

    Update 199A - Hexadecimal's theorem.cpp

--- 199A - Hexadecimal's theorem.cpp
+++ 199A - Hexadecimal's theorem.cpp
@@ -1,7 +1,63 @@
-#include <iostream>
+#include<bits/stdc++.h>
 using namespace std;
-int a;
-int main() {
-    cin >> a;
-    cout << 0 << " " << 0 << " " << a;
+int main()
+{
+    int n;
+    cin>>n;
+    
+    if(n==3)
+    {
+        cout<<"1"<<" "<<"1"<<" "<<"1";
+        return 0;
+    }
+    
+      if(n==2)
+    {
+        cout<<"0"<<" "<<"1"<<" "<<"1";
+        return 0;
+    }
+       if(n==1)
+    {
+        cout<<"0"<<" "<<"0"<<" "<<"1";
+        return 0;
+    }
+       if(n==0)
+    {
+        cout<<"0"<<" "<<"0"<<" "<<"0";
+        return 0;
+    }
+    int fb[n+1];
+    
+    fb[0]=0;
+    fb[1]=1;
+    for(int i=2;i<=n;i++)
+    {
+        fb[i]=fb[i-1]+fb[i-2];
+    }
+    bool flage = false;
+    for(int i=0;i<=n;i++)
+    {
+        if(fb[i]==n)
+        {
+            flage =true;
+            break;
+        }
+    }
+    if(flage==false)
+    {
+        cout<<" not fibonacci";
+        return 0;
+    }else
+    {
+        for(int i=5;i<=n;i++)
+        {
+            if(fb[i]==n)
+            {
+                cout<<fb[i-4]<<" "<<fb[i-3]<<" "<<fb[i-1];
+                return 0;
+            }
+        }
+    }
+    
+    return 0;
 }
